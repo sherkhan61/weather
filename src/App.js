742b30c667b0728d9048091b5f1d936d46c715aa
sherkhan61 <65385144+sherkhan61@users.noreply.github.com>
2021-03-16 22:48:12 +0600

    adding header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <h2 className="title"><i className="fa fa-cloud"></i>Weather App</h2>
+          <div className="search-form">
+              <input type="text" placeholder="Enter your city name"/>
+              <button type="button">Search</button>
+          </div>
+
+      </div>
     </div>
   );
 }
